dfde009eb5cea434f14bc61d01435c4cc65857ed
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2017-06-23 20:28:10 +0300

    First working version with circular array

--- main.c
+++ main.c
@@ -7,6 +7,93 @@
 #define SNAKE_INITIAL_SIZE 5
 #define SNAKE_LOOKS ACS_DIAMOND
 
+typedef struct {
+    int x, y;
+} SnakeBlock;
+
+typedef struct {
+    int size;
+    int capacity;
+    int head;
+    int tail;
+    SnakeBlock block[SNAKE_MAX_SIZE];
+} Snake;
+
+/* Initialize the snake starting from pos (x, y) */
+static void snake_init(Snake *snake, int x, int y) {
+    snake->size = SNAKE_INITIAL_SIZE;
+    snake->capacity = SNAKE_MAX_SIZE;
+    snake->head = SNAKE_INITIAL_SIZE - 1;
+    snake->tail = 0;
+    for (int i = 0; i < snake->size; i++) {
+	snake->block[i].x = x;
+	snake->block[i].y = y--;
+    }
+}
+
+/* Move snake head to a new position, advancing the tail as well.
+ The old tail position is returned to the calling function. */
+static SnakeBlock snake_new_head_pos(Snake *snake, int newx, int newy) {
+    SnakeBlock oldtail;
+
+    /* Handle the head. */
+    if (snake->head == snake->capacity)
+	/* Reached the end of buffer list, start from the beginning. */
+	snake->head = 0;
+    else 
+	snake->head++;
+    snake->block[snake->head].x = newx;
+    snake->block[snake->head].y = newy;    
+    
+    /* Handle the tail. */
+    oldtail.x = snake->block[snake->tail].x;
+    oldtail.y = snake->block[snake->tail].y;
+    if (snake->tail == snake->capacity)
+	snake->tail = 0;
+    else
+	snake->tail++;
+    
+    return oldtail;
+}
+
+static SnakeBlock snake_head_coordinates(Snake *snake) {
+    SnakeBlock head;
+
+    head.x = snake->block[snake->head].x;
+    head.y = snake->block[snake->head].y;
+
+    return head;
+}
+
+static void snake_paint_block(SnakeBlock block, chtype looks) {
+    mvaddch(block.x, block.y, looks);
+}
+
+static void snake_paint(Snake *snake) {
+    int start1, stop1, start2, stop2;
+
+    start1 = stop1 = stop2 = 0;
+    start2 = 1;			/* Don't do extra painting if not needed. */
+
+    if (snake->head > snake->tail) {
+	start1 = snake->tail;
+	stop1 = snake->head;
+    }
+    else {
+	/* Two sections to paint, one at the start, one at the end of the array */
+    	start1 = 0;
+    	stop1 = snake->head;
+        start2 = snake->tail;
+    	stop2 = snake->capacity - 1;
+    }
+    
+    for (int i = start1; i <= stop1; i++)
+	snake_paint_block(snake->block[i], SNAKE_LOOKS);
+    for (int i = start2; i <= stop2; i++)
+	snake_paint_block(snake->block[i], SNAKE_LOOKS);
+    refresh();
+}
+
 typedef enum {
     SNAKE_LEFT,
     SNAKE_RIGHT,
@@ -15,25 +102,16 @@ typedef enum {
     SNAKE_UNKNOWN
 } snake_direction_enum;
 
-typedef struct {
-    int x, y;
-    snake_direction_enum neighbour;
-} snake_block_st;
-
-typedef struct {
-    int size;
-    snake_block_st block[SNAKE_MAX_SIZE];
-} snake_st;
 
 static snake_direction_enum snake_direction;
 static int should_exit;
 
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
-static void *thread_func(void *arg) {
+static void *snake_update_direction(void *arg) {
     int ch;
     int s;
-
+    
     while(should_exit != 1) {
 	ch = getch();
 	if ((s = pthread_mutex_lock(&mtx)) != 0)
@@ -46,11 +124,11 @@ static void *thread_func(void *arg) {
 	    break;
 	case KEY_RIGHT:
 	    if (snake_direction != SNAKE_LEFT)
-	    snake_direction = SNAKE_RIGHT;
+		snake_direction = SNAKE_RIGHT;
 	    break;
 	case KEY_UP:
 	    if (snake_direction != SNAKE_DOWN)
-	    snake_direction = SNAKE_UP;
+		snake_direction = SNAKE_UP;
 	    break;
 	case KEY_DOWN:
 	    if (snake_direction != SNAKE_UP)
@@ -66,15 +144,11 @@ static void *thread_func(void *arg) {
         
 	if ((s = pthread_mutex_unlock(&mtx)) != 0)
 	    exit(1);
+	
+	usleep(100000);		/* Avoid goind backwards by tapping keys in quick succession. */
     }
     
     return (void *)0;
-}
-
-static void snake_paint(snake_st *snake) {
-    for (int i = 0; i < snake->size; i++)
-    	mvaddch(snake->block[i].x, snake->block[i].y, SNAKE_LOOKS);
-    refresh();
 }
 
 int main() {
@@ -83,7 +157,7 @@ int main() {
     int s, ch, will_exit;
     int row, col;
 
-    snake_st snake;
+    Snake snake;
     snake_direction_enum direction;
 
     /* General initializations */
@@ -100,72 +174,60 @@ int main() {
     getmaxyx(stdscr, row, col);	/* Get window size */
 
     /* Initialize the snake */
-    snake.size = 0;
-    for (int i = 0; i < SNAKE_INITIAL_SIZE; i++) {
-	snake.block[i].x = row/2;
-	snake.block[i].y = col/2 - i;
-	snake.block[i].neighbour = SNAKE_LEFT;
-	snake.size++;
-    }
-    
-    snake_paint(&snake);	/* Initial snake. */
+    snake_init(&snake, row/2, col/2);
+    snake_paint(&snake);
 
     /* Watch for keyboard input. */
-    if ((s = pthread_create(&t1, NULL, thread_func, "This is on thread")) != 0)
+    if ((s = pthread_create(&t1, NULL, snake_update_direction, NULL)) != 0)
     	exit(1);
 
     /* Main loop. */
     while (will_exit != 1) {
-	/* Maybe the user has changes the snake direction or wants to go home */
-	if ((s = pthread_mutex_lock(&mtx)) != 0)
-	    exit(1);
+    	/* Maybe the user has changes the snake direction or wants to go home */
+    	if ((s = pthread_mutex_lock(&mtx)) != 0)
+    	    exit(1);
         direction = snake_direction;
-	will_exit = should_exit;
-	if ((s = pthread_mutex_unlock(&mtx)) != 0)
-	    exit(1);
+    	will_exit = should_exit;
+    	if ((s = pthread_mutex_unlock(&mtx)) != 0)
+    	    exit(1);
 
-	switch(direction) {
-	case SNAKE_LEFT:
-	    for (int i = 0; i < snake.size; i++) {
-		mvaddch(snake.block[i].x, snake.block[i].y, ' ');
-		snake.block[i].y--;              
-	    }
-	    break;
-	case SNAKE_RIGHT:
-	    for (int i = 0; i < snake.size; i++) {
-		mvaddch(snake.block[i].x, snake.block[i].y, ' ');
-		snake.block[i].y++;
-	    }
-	    break;
-	case SNAKE_UP:
-	    for (int i = 0; i < snake.size; i++) {
-		mvaddch(snake.block[i].x, snake.block[i].y, ' ');
-		snake.block[i].x--;
-	    }
-	    break;
-	case SNAKE_DOWN:
-	    for (int i = 0; i < snake.size; i++) {
-		mvaddch(snake.block[i].x, snake.block[i].y, ' ');
-		snake.block[i].x++;
-	    }
-	    break;
-	}
+	SnakeBlock head, tail;
+	head = snake_head_coordinates(&snake);
+	int nextx, nexty;
 
+    	switch(direction) {
+    	case SNAKE_LEFT:
+            nextx = head.x;
+	    nexty = head.y - 1;
+    	    break;
+    	case SNAKE_RIGHT:
+            nextx = head.x;
+	    nexty = head.y + 1;
+    	    break;
+    	case SNAKE_UP:
+            nextx = head.x - 1;
+	    nexty = head.y;
+    	    break;
+    	case SNAKE_DOWN:
+            nextx = head.x + 1;
+	    nexty = head.y;
+    	    break;
+    	}
+
+        tail = snake_new_head_pos(&snake, nextx, nexty);
 	snake_paint(&snake);
-	/* Update head */
-	/* mvaddch(snake.headx, snake.heady, SNAKE_LOOKS); */
-	/* refresh(); */
-
-	/* Kill tail */
-	/* mvaddch(snake.tailx, --snake.taily, ' '); */
-
+	snake_paint_block(tail, ' '); /* Tail erase. */
+	/* mvprintw(25, 0, "Old: %d, %d", oldtail.x, oldtail.y); */
+	refresh();
+        
 	usleep(100000);
     }
-    
+
     if ((s = pthread_join(t1, &res)) != 0)
     	exit(1);
 
     endwin();
+ 
     return 0;
 }
 
